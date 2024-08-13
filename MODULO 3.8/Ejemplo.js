56187382e0099e5d413c712ca0c836e5e11fcc6b
crlitos1-3 <115955965+crlitos1-3@users.noreply.github.com>
2024-08-13 08:40:33 -0600

    Delete MODULO 3.8 directory

--- MODULO 3.8/Ejemplo.js
+++
@@ -1,7 +0,0 @@
-var peso, edad;
-//Capturando Datos 
-peso=prompt("Ingrese su peso");
-edad=prompt("Ingrese su edad");
-document.write("La edad Ingresesada es:"+edad+"<br>");//Lo muestra detro del documento 
-alert("El peso Ingresado es:"+peso+"<br>");//Lo muestra como un cuadro de dialogo 
-
--- MODULO 3.8/domcito.js
+++
@@ -1,8 +0,0 @@
-function calcular()
-{
-    let nombre=document.getElementById('name1').value;
-    let edad=parseInt(document.getElementById('edad1').value);
-    let aumento;
-    aumento=edad+10;
-    alert("su edad aumentada es"+aumento)
-}
